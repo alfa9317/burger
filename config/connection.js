2bab42bb0f57b81340dbd98353a339c27eb2357a
alfa9317 <josealfredotg@gmail.com>
2019-12-11 19:33:23 -0600

    conection.js file modification

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: process.env.SUPER_SECRET_PASSWORD,
+  password: `${process.env.SUPER_SECRET_PASSWORD}`,
   database: "cat_db"
 });
 
