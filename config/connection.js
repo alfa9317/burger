43dc85a5ef3e179f4716ef56fa7d356c1358c514
alfa9317 <josealfredotg@gmail.com>
2019-12-12 14:28:35 -0600

    JAWSDB configuration

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 var mysql = require("mysql");
 require("dotenv").config();
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: `${process.env.SUPER_SECRET_PASSWORD}`,
-  database: "burgers_db"
-});
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: `${process.env.SUPER_SECRET_PASSWORD}`,
+        database: "burgers_db"
+      });
+};
+
 
 connection.connect(function(err) {
   if (err) {
