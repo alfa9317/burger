eebaee622760b88b58e83b63484949d7ada63e35
alfa9317 <josealfredotg@gmail.com>
2019-12-12 12:15:05 -0600

    First version of the app already working

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: `${process.env.SUPER_SECRET_PASSWORD}`,
-  database: "cat_db"
+  database: "burgers_db"
 });
 
 connection.connect(function(err) {
